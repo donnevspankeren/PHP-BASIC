e4142ed2e4fde7815d36c93eac9fa7886ce32422
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-05-13 12:42:31 +0200

    variable-datatype taak8

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,11 @@
+<DOCTYPE! html>
+<html>
+<ul>
+    <?php
+    $voornaam1 = "njoy";
+    $voornaam2 = "jared";
+    $voornaam3 = "luca";
+    echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+    ?>
+</ul>
+</html>
