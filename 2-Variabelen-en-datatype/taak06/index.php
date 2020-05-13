c8b0034c1778ec5e3ddd9fc6a98d1308b82b419c
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-05-13 12:33:53 +0200

    variable-datatype taak 6

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,12 @@
+<?php
+$string = "hallo";
+$integer = 10;
+$float = 2.3;
+$boolean = false;
+$leeg = null;
+
+echo gettype($string) ."<br>". gettype($integer) ."<br>". gettype($float) ."<br>". gettype($boolean) ."<br>". gettype($leeg);
+
+
+
+?>
