45cbdd0c858ac0947f68ad7966ea1e9ca0784055
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-05-13 12:37:32 +0200

    taak 7 goede versie

--- 2-Variabelen-en-datatype/taak07/index.php
+++ 2-Variabelen-en-datatype/taak07/index.php
@@ -2,6 +2,6 @@
 $voornaam = "Donne";
 $leeftijd = 17;
 
-echo "Mijn naam is ". $voornaam. " en ik ben ". $leeftijd . " jaar oud.";
+echo "Mijn naam is $voornaam en ik ben  $leeftijd jaar oud.";
 
 ?>
