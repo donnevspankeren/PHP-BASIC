1f79988c3383deb061077dee71f3980fd5fd7bbc
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-05-14 11:51:11 +0200

    Conditional_Statements taak2

---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,25 @@
+<?php
+$test1 = 10;
+$test2 = 6;
+if($test1 == $test2){
+    echo "Gelijk" . "<br>";
+}
+if($test1 != $test2){
+    echo "Ongelijk" . "<br>";
+}
+if($test1 === $test2){
+    echo "Identiek" . "<br>";
+}
+if($test1 > $test2){
+    echo "Test 1 is groter dan test 2." . "<br>";
+}
+if($test1 < $test2){
+    echo "test 1 is kleiner dan test 2." . "<br>";
+}
+if($test1 >= $test2){
+    echo "Test 1 is groter of gelijk aan test 2." . "<br>";
+}
+if($test1 <= $test2){
+    echo "Test 1 is kleiner of gelijk aan test 2." . "<br>";
+}
+?>
