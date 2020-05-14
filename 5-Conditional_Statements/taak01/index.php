3227fa4b784bd7a3fd0193906afbea7a31257415
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-05-14 11:41:13 +0200

    Conditional_Statements taak1

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,20 @@
+<?php
+$test1 = 12;
+$test2 = "hallo";
+
+var_dump($test1 == $test2);
+echo "<br>";
+var_dump($test1 === $test2);
+echo "<br>";
+var_dump($test1 != $test2);
+echo "<br>";
+var_dump($test1 > $test2);
+echo "<br>";
+var_dump($test1 < $test2);
+echo "<br>";
+var_dump($test1 <> $test2);
+echo "<br>";
+var_dump($test1 >= $test2);
+echo "<br>";
+var_dump($test1 <= $test2);
+?>
