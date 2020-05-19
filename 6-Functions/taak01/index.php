03b9c8d2b2c177dd58db6e58b4c8d1e947a1ec5c
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-05-19 11:34:12 +0200

    functions taak 1

---
+++ 6-Functions/taak01/index.php
@@ -0,0 +1,10 @@
+<?php
+function zetTweeWoordenAanElkaar(){
+    $woord = "hallo";
+    $zinnetje = " Mooie blauw planeet";
+    $helezin = $woord . $zinnetje;
+    echo $helezin;
+}
+
+zetTweeWoordenAanElkaar();
+?>
